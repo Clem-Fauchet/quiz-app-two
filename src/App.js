db9b61373e0537a646219b28b984b75c5f6d6edd
Clem <clemence.fauchet23@gmail.com>
2020-02-10 15:14:32 +0100

    Adding selected answer and error if no answer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Answers from './components/Answers'
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [currentAnswer, setCurrentAnswer] = useState('')
+  const [answers, setAnswers] = useState([])
+
+  const [error, setError] = useState('')
 
   const questions = [
     {
@@ -42,12 +45,48 @@ function App() {
 
   const question = questions[currentQuestion]
 
+  const handleClick = (e) => {
+    setCurrentAnswer(e.target.value)
+    setError('')
+  }
+
+  const renderError = () => {
+    if (!error) {
+      return
+    }
+    return <div className='error'>{error}</div>
+  }
+
+  const next = () => {
+    const answer = { questionId: question.id, answer: currentAnswer }
+
+    if (!currentAnswer) {
+      setError('Please select an option before continuing')
+      return
+    }
+
+    answers.push(answer)
+    setAnswers(answers)
+    setCurrentAnswer('')
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    }
+  }
+
   return (
     <div className='container'>
-      <Progress total='3' current='1' />
+      <Progress total={questions.length} current={currentQuestion + 1} />
       <Question question={question.question} />
-      <Answers question={question} currentAnswer={currentAnswer} />
-      <button className='btn btn-primary'>Confirm and continue</button>
+      {renderError()}
+      <Answers
+        question={question}
+        currentAnswer={currentAnswer}
+        handleClick={handleClick}
+      />
+      <button className='btn btn-primary' onClick={next}>
+        Confirm and continue
+      </button>
     </div>
   )
 }
--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,11 +4,16 @@ function Answer(props) {
   let classes = ['answer']
 
   if (props.selected) {
-    classes.push('selected')
+    classes.push(' selected')
   }
   return (
     <div>
-      <button className='btn' className={classes.join('')} value={props.letter}>
+      <button
+        className='btn'
+        className={classes.join('')}
+        value={props.letter}
+        onClick={props.handleClick}
+      >
         <span className='letter'>{props.letter}</span> {props.answer}
       </button>
     </div>
